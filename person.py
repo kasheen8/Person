b038a6a3fecc403ffaae54f3b73c571dd9fdeea1
kasheen8 <abinaevravil@mail.ru>
2019-12-26 02:05:58 +0400

    Изучение принципов ООП

---
+++ person.py
@@ -0,0 +1,62 @@
+from classtools import AttrDisplay
+class Person(AttrDisplay):
+    """
+    Создает и обрабатывает записи с информацией о людях
+    """
+    def __init__(self, name, job=None, pay=0):
+        self.name = name
+        self.job = job
+        self.pay = pay
+    def lastName(self):                 #предполагается что фамилия
+        return self.name.split()[-1]    #указана последней
+    def giveRaise(self, percent):       #процент - величина в диапозоне 0..1
+        self.pay = int(self.pay * (1+percent))
+
+class Manager(Person):
+    """
+    Версия класса Person, адаптированная в соответствии
+    со специальными требованиями
+    """
+    def __init__(self, name, pay):
+        Person.__init__(self,name, 'mgr', pay)
+
+    def giveRaise(self, percent, bonus = .10):
+        Person.giveRaise(self,percent + bonus)
+
+if __name__ == '__main__':
+    bob = Person('Bob Smith')
+    sue = Person('Sue Jones',job='dev',pay=100000)
+
+    print(bob)
+    print(sue)
+    print(bob.lastName(), sue.lastName())
+    sue.giveRaise(.10)
+    print(sue.pay)
+    print(sue)
+
+    tom = Manager('Tom Jones', 50000)
+    tom.giveRaise(.10)
+    print(tom.lastName())
+    print(tom)
+    print('--All three--')
+    for object in (bob,sue,tom):
+        object.giveRaise(.10)
+        print(object)
+
+#class Department:
+#    def __init__(self,*args):
+#        self.members = list(args)
+#    def addMember(self,person):
+#        self.members.append(person)
+#    def giveRaise(self,percent):
+#        for person in self.members:
+#            person.giveRaise(percent)
+#    def showAll(self):
+#        for person in self.members:
+#            print(person)
+
+#development = Department(bob, sue)
+#development.addMember(tom)
+#development.giveRaise(.10)
+#development.showAll()
+
